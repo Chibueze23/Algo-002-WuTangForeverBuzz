6922dc29470ba8899ea40aa605869c5bfad32e94
Chibueze Anyadike <Chibueze_23@Chibuezes-MacBook-Air.local>
2019-07-30 10:27:36 -0400

    Done

--- src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
@@ -3,6 +3,20 @@ package hbcu.stay.ready.algorithms;
 public class WuTangForeverBuzz {
 
     public String wuTangClan(){
-        return null;
+        String result = "";
+        for(int x = 1; x<= 15; x++){
+            if (x % 3 == 0 && x % 5 == 0){
+                System.out.println("Wutang Forever");
+            } else if (x % 3 == 0){
+                System.out.println("Wu");
+            } else if (x % 5 == 0) {
+                System.out.println("Tang");
+            }
+            else {
+                System.out.println(x);
+            }
+            result += x;
+        }
+        return result;
     }
 }
